79aa08de73b0946cb54f79a95dda90eda204f5f9
richard <binghamrichard123@gmail.com>
2023-07-12 12:12:25 +0100

    improved formatting and checked psr-12

--- php/functions.php
+++ php/functions.php
@@ -1,16 +1,15 @@
 <?php
 
-function songLoop() {
-    for($i = 99; $i >= 0; $i--) {
+function songLoop()
+{
+    for ($i = 99; $i >= 0; $i--) {
 
-        $i > 1 ? $j = $i -1 : $j = 'no more';
+        $j = $i > 1 ? $i -1 :'no more';
+        $bottle1 = $i == 1 ? 'bottle' : 'bottles';
+        $bottle2 = $j == 1 ? 'bottle' : 'bottles';
 
-        $i == 1 ? $bottle1 = 'bottle' : $bottle1 = 'bottles';
-
-        $j == 1 ? $bottle2 = 'bottle' : $bottle2 = 'bottles';
-
-        if($i > 0) {
-            echo "<p>$i $bottle1 of beer on the wall, $i $bottle1 of beer. 
+        if ($i > 0) {
+            echo "<p>$i $bottle1 of beer on the wall, $i $bottle1 of beer.
             Take one down and pass it around, $j $bottle2 of beer on the wall.</p>";
         } else {
             echo "<p>No more bottles of beer on the wall, no more bottles of beer.
