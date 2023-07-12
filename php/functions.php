d74cc31071aa32fbde4980c3fcd1bd954e4c1d2c
richard <binghamrichard123@gmail.com>
2023-07-12 12:24:12 +0100

    a little more formatting

--- php/functions.php
+++ php/functions.php
@@ -9,11 +9,12 @@ function songLoop()
         $bottle2 = $j == 1 ? 'bottle' : 'bottles';
 
         if ($i > 0) {
-            echo "<p>$i $bottle1 of beer on the wall, $i $bottle1 of beer.
-            Take one down and pass it around, $j $bottle2 of beer on the wall.</p>";
+            echo "<p>$i $bottle1 of beer on the wall, $i $bottle1 of beer.</p>" .
+            "<p>Take one down and pass it around, $j $bottle2 of beer on the wall.</p>" .
+            "<br>";
         } else {
-            echo "<p>No more bottles of beer on the wall, no more bottles of beer.
-            Go to the store and buy some more, 99 bottles of beer on the wall.</p>";
+            echo "<p>No more bottles of beer on the wall, no more bottles of beer.</p>" .
+            "<p>Go to the store and buy some more, 99 bottles of beer on the wall.</p>";
         }
     }
 }
